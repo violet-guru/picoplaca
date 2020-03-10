c9211fd346e7e2a850eab170937f1f4fb30f510f
PC-01 <v99h@hotmail.com>
2020-03-10 16:11:50 -0500

    Readind the plate and the date

--- index.js
+++ index.js
@@ -1,17 +1,47 @@
-const readline = require('readline');
-const rl = readline.createInterface({input: process.stdin, output: process.stdout});
+const readline = require('readline')
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
 
-const readPlate = (plateValue) => {
-    rl.question('Insert the date (YYYY/MM/DD):', readDate);
+const carData = {}
+
+const initRead = (actualMessage) => {
+  rl.question((actualMessage || '') + 'Insert the plate number (AAA-9999 or AAA-999):', readPlate)
 }
 
-const readDate = (dateValue) => {
-    rl.question('Insert the time (HH:MM):', readTime);
+const isValidPlate = (plateValue) => {
+  return /^[A-Za-z]{3}[\-][0-9]{3,4}$/.test(plateValue)
+}
+
+const readPlate = (plateValue, actualMessage) => {
+  if (!isValidPlate(plateValue)) {
+    initRead('Try again. ')
+    return
+  }
+
+  carData.plateText = plateValue
+  initDate('')
+}
+
+const initDate = (actualMessage) => {
+  rl.question((actualMessage || '') + 'Insert the date (YYYY/MM/DD):', readDate)
+}
+
+const readDate = (dateValue, actualMessage) => {
+  const myDate = moment(dateValue)
+
+  if (!myDate.isValid()) {
+    initDate('Try again. ')
+    return
+  }
+
+initTime('')
+}
+
+const initTime = (actualMessage) => {
+  rl.question((actualMessage || '') + 'Insert the time (HH:MM):', readTime)
 }
 
 const readTime = (timeValue) => {
-    rl.close();
+  rl.close()
 }
 
-
-rl.question('Insert the plate number:', readPlate);
+initRead('')
