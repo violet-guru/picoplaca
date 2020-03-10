9a9ba0ba05731a7b5a4203359910af411bf32dcc
PC-01 <v99h@hotmail.com>
2020-03-10 18:09:52 -0500

    Running automated tests before git push, with Husky

---
+++ index.js
@@ -0,0 +1,9 @@
+// Victor Hugo Lafuente Cevallos
+//
+// Node console app. Run with 'npm start'
+// Tests made with Jest in index.jest.js
+// Packages [readline] and [moment] have they own unit tests.
+// Running automated tests before git push, with Husky
+
+const pico = require('./HasPico')
+pico.InitReadPlate()
--- index.test.js
+++ index.test.js
@@ -4,67 +4,67 @@ const moment = require('moment')
 const pico = require('./HasPico')
 
 it('uses an available datetime for the car.', () => {
-  //Arrange
-  let expected = false;
+  // Arrange
+  const expected = false
 
-  //Act
-  let result = pico.HasProhibition('aaa-997', moment('2020-03-10 08:00'))
+  // Act
+  const result = pico.HasProhibition('aaa-997', moment('2020-03-10 08:00'))
 
-  //Assert
-  expect(result).toBe(expected);
+  // Assert
+  expect(result).toBe(expected)
 })
 
 it('uses an available datetime for the car at night.', () => {
-  //Arrange
-  let expected = false;
+  // Arrange
+  const expected = false
 
-  //Act
-  let result = pico.HasProhibition('aaa-990', moment('2020-03-10 22:00'))
+  // Act
+  const result = pico.HasProhibition('aaa-990', moment('2020-03-10 22:00'))
 
-  //Assert
-  expect(result).toBe(expected);
+  // Assert
+  expect(result).toBe(expected)
 })
 
 it('uses a prohibited datetime for the car.', () => {
-  //Arrange
-  let expected = true;
+  // Arrange
+  const expected = true
 
-  //Act
-  let result = pico.HasProhibition('aaa-999', moment('2020-03-13 19:00'))
+  // Act
+  const result = pico.HasProhibition('aaa-999', moment('2020-03-13 19:00'))
 
-  //Assert
-  expect(result).toBe(expected);
+  // Assert
+  expect(result).toBe(expected)
 })
 
 it('uses an available datetime for the car in pico and placa day, but lunchtime.', () => {
-  //Arrange
-  let expected = false;
+  // Arrange
+  const expected = false
 
-  //Act
-  let result = pico.HasProhibition('aaa-998', moment('2020-03-12 13:00'))
+  // Act
+  const result = pico.HasProhibition('aaa-998', moment('2020-03-12 13:00'))
 
-  //Assert
-  expect(result).toBe(expected);
+  // Assert
+  expect(result).toBe(expected)
 })
 
 it('checks a valid plate number.', () => {
-  //Arrange
-  let expected = true;
+  // Arrange
+  const expected = true
 
-  //Act
-  let result = pico.IsValidPlate('PBC-998')
+  // Act
+  const result = pico.IsValidPlate('PBC-998')
 
-  //Assert
-  expect(result).toBe(expected);
+  // Assert
+  expect(result).toBe(expected)
 })
 
 it('checks a non valid car plate number.', () => {
-  //Arrange
-  let expected = false;
+  // Arrange
+  const expected = false
 
-  //Act
-  let result = pico.IsValidPlate('PBC998')
+  // Act
+  const result = pico.IsValidPlate('PBC998')
 
-  //Assert
-  expect(result).toBe(expected);
+  // Assert
+  expect(result).toBe(expected)
 })
