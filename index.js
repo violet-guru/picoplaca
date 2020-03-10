623740e841874fbf5df6771a70f79bc03340f67a
PC-01 <v99h@hotmail.com>
2020-03-10 16:31:23 -0500

    Finalized user input in console

--- index.js
+++ index.js
@@ -1,46 +1,55 @@
 const readline = require('readline')
+const moment = require('moment')
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
 
-const carData = {}
-
 const initRead = (actualMessage) => {
-  rl.question((actualMessage || '') + 'Insert the plate number (AAA-9999 or AAA-999):', readPlate)
+  rl.question((actualMessage || '') + 'Insert the plate number in the format AAA-9999 or AAA-999 >', readPlate)
 }
 
 const isValidPlate = (plateValue) => {
   return /^[A-Za-z]{3}[\-][0-9]{3,4}$/.test(plateValue)
 }
 
-const readPlate = (plateValue, actualMessage) => {
+const readPlate = (plateValue) => {
   if (!isValidPlate(plateValue)) {
     initRead('Try again. ')
     return
   }
 
-  carData.plateText = plateValue
-  initDate('')
+  initDate('', {plateText: plateValue})
 }
 
-const initDate = (actualMessage) => {
-  rl.question((actualMessage || '') + 'Insert the date (YYYY/MM/DD):', readDate)
+const initDate = (actualMessage, carData) => {
+  rl.question((actualMessage || '') + 'Insert the date in the format YYYY/MM/DD >', (answer) => {readDate(answer, carData)})
 }
 
-const readDate = (dateValue, actualMessage) => {
-  const myDate = moment(dateValue)
+const readDate = (dateValue, carData) => {
+  const myDate = moment(dateValue, 'YYYY/MM/DD', true)
 
   if (!myDate.isValid()) {
-    initDate('Try again. ')
+    initDate('Try again. ', carData)
     return
   }
 
-initTime('')
+  carData.dateValue = myDate
+  initTime('', carData)
 }
 
-const initTime = (actualMessage) => {
-  rl.question((actualMessage || '') + 'Insert the time (HH:MM):', readTime)
+const initTime = (actualMessage, carData) => {
+  rl.question((actualMessage || '') + 'Insert the time in the format HH:MM >', (answer) => {readTime(answer, carData)})
 }
 
-const readTime = (timeValue) => {
+const readTime = (timeValue, carData) => {
+  const oldDate = carData.dateValue.format('YYYY/MM/DD')
+  const myDate = moment(`${oldDate} ${timeValue}`, 'YYYY/MM/DD HH:mm', true)
+
+  if (!myDate.isValid()) {
+    initTime('Try again. ', carData)
+    return
+  }
+
+  carData.dateValue = myDate
+  console.log(carData)
   rl.close()
 }
 
